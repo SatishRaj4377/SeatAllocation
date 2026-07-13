c462f62f977f9df1083b26f2b4547219e3371ee3
SatishRaj4377 <158574671+SatishRaj4377@users.noreply.github.com>
2026-07-13 19:41:10 +0530

    created separated page for the eymard and mathura

---
+++ Pages/Eymard.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SeatAllocation.Pages;
+
+public class EymardModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
---
+++ Pages/Mathura.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SeatAllocation.Pages;
+
+public class MathuraModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
